a2db78ecd4061cc171807880f4202bc7a2296e15
BenS <bshalev1@gmail.com>
2023-12-21 11:21:10 +0200

    submission

---
+++ home work 1/AddTwo.java
@@ -0,0 +1,13 @@
+/*
+ * Adds two given integers and prints the result in a fancy way.
+ */
+public class AddTwo {
+	public static void main(String[] args) {
+		int x = Integer.parseInt(args[0]);
+		int y = Integer.parseInt(args [1]);
+		int sum;
+		sum = x + y;
+		System.out.println(x + " + " + y + " = " + sum);
+		// Put your code here	
+	}
+}
---
+++ home work 1/LinearEq.java
@@ -0,0 +1,19 @@
+/*
+ * Solves linear equations of the form a⋅x + b = c.
+ * The program gets a, b, and c as command-line arguments,
+ * computes x, and prints the result.
+ * Treats the three arguments as well as the computed value as double values
+ */
+
+ public class LinearEq {
+	public static void main(String[] args) {
+	double a = Double.parseDouble(args[0]);
+	double b = Double.parseDouble(args[1]);
+	double c = Double.parseDouble(args[2]);
+	double x = c-b;
+	double sum =   x/a;
+	System.out.println(sum);
+
+	// Put your code here
+}
+}
