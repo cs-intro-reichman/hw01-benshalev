c267f684a5b62ef2f118196b180ca27d0bd2f8dc
BenS <bshalev1@gmail.com>
2023-12-24 19:45:00 +0200

    home work

--- LinearEq.java
+++ LinearEq.java
@@ -10,10 +10,9 @@
 	double a = Double.parseDouble(args[0]);
 	double b = Double.parseDouble(args[1]);
 	double c = Double.parseDouble(args[2]);
-	double x = c-b;
-	double sum =   x/a;
+	double x = (c- b) / a;
 	System.out.println(a + " * x +" + b + " = " + c);
-	System.out.println("x = " + sum);
+	System.out.println("x = " + x);
 	// Put your code here
 }
 }
