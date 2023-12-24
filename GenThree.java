1501372a709719124302319d35a253125e344f2f
BenS <bshalev1@gmail.com>
2023-12-24 19:59:44 +0200

    home work

--- GenThree.java
+++ GenThree.java
@@ -12,11 +12,10 @@ public class GenThree {
 		int x = ((int)(Math.random() *(b - a) + a));
 		int y = ((int)(Math.random() *(b - a) + a));
 		int z = ((int)(Math.random() *(b - a) + a));
-		int min = Math.min(Math.min(x, y),z);
-		System.out.println(x) ;
-		System.out.println(y) ;
-		System.out.println(z) ;
-		System.out.println("The minimal generated number is " + min) ;
-		// Put your code here	
+		int min = Math.min(Math.min(x,y),z);
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println("The minimal generated number is " + min) ;	
 	}
 }
